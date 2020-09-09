b37f75e6f35c847acbf36c0517c06daad8fe656b
Taylor Miles <taylormiles19@gmail.com>
2020-09-09 16:17:55 -0700

    Removed API key...oops

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ ACCOUNT_ID = 'NExH5gkVThJbxHhhfOLxh-82Zwc_ONAV6hmOjbk25y_S2UM'
 
 def main():
 
+    account_names = ['Traiyn', 'PVLeviathan']
+
     with open('secret.json') as f:
         api_key = json.load(f)
-    # resp = requests.get('https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/Traiyn?api_key=RGAPI-a6a97a8a-bacd-47ff-a5bb-f42ba6c551a6')
-    # resp = requests.get('https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-a6a97a8a-bacd-47ff-a5bb-f42ba6c551a6')
-    # resp = requests.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Traiyn?api_key=RGAPI-a6a97a8a-bacd-47ff-a5bb-f42ba6c551a6')
-    resp = requests.get(f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{ACCOUNT_ID}?api_key={api_key['key']}")
+
+    for name in account_names:
+
+    resp = requests.get(f"https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/Traiyn?api_key={api_key['key']}")
+    # resp = requests.get(f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{ACCOUNT_ID}?api_key={api_key['key']}")
 
     print(resp.text)
 
