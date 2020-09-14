3670dfa3daf11bd04672fca562e631881b96621b
Taylor Miles <taylormiles19@gmail.com>
2020-09-14 16:01:18 -0700

    Reset to django rest framework

--- backend/src/abcdtft/asgi.py
+++ backend/src/abcdtft/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for abcdtft_site project.
+ASGI config for abcdtft project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abcdtft_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abcdtft.settings')
 
 application = get_asgi_application()
--- src/summoners/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SummonersConfig(AppConfig):
-    name = 'summoners'
