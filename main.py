33440f06abc19a8ceff1ab477bb0c9cb64f8e6af
Taylor Miles <taylormiles19@gmail.com>
2022-04-29 13:39:45 -0700

    Fixed rate-limiting. Cleaned up main.py.

--- main.py
+++ main.py
@@ -9,89 +9,15 @@ from api_config import ApiConfig
 import riot_api
 import time
 
-REGION = "NA"
 SUMMONER = "Traiyn"
-RATE_LIMIT = {
-    'per_second': 20,
-    'per_minute': 100
-}
-
-RIOT_API_URL_BASE = "https://na1.api.riotgames.com/"
-RIOT_API_URL_TFT = RIOT_API_URL_BASE + "tft/"
-
-def get_summoner_info():
-    pass
-
 
 def main():
 
     # summoner = input("Please enter a summoner's name: ")
-
-
-    riot_api_config = ApiConfig(riot_api_key, RATE_LIMIT)
-    puuid = riot_api.get_summoner_id(riot_api_config, SUMMONER)
-    match_history = riot_api.get_summoner_match_history(riot_api_config, puuid)
-
-    # move below to riot_api_py
-    riot_api_url_tftmatch = "https://americas.api.riotgames.com/tft/match/v1/matches/"
-    existing_matches = os.listdir(os.path.join('data', SUMMONER))
-    counter = 0
-
-    for match in match_history:
-        match_filename = match + '.json'
-        if match_filename in existing_matches:
-            continue
-        if counter == 99:
-            print('Abiding by API rate limits...')
-            time.sleep(60) # 100 per minute
-            print('Continuing')
-            counter = 0
-            
-        request_url = riot_api_url_tftmatch + match + '?api_key=' + riot_api_config.api_key
-        resp = requests.get(request_url)
-        match_info = resp.json()
-        filename = match + '.json'
-        filepath = os.path.join('data', SUMMONER, filename)
-        if not os.path.exists(filepath):
-            with open(filepath, 'w') as f:
-                json.dump(match_info, f)
-        counter += 1
-
-def sub_test(api_key):
-    
-    account_names = ['Traiyn']
-
-    for name in account_names:
-        # Get account info
-        resp = requests.get(f"https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/{name}?api_key={api_key['key']}")
-        account_id = resp.json()['accountId']
-        puu_id = resp.json()['puuid']
-        # Get match history
-        resp = requests.get(f"https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?api_key={api_key['key']}")
-        # resp = requests.get(f"https://na1.api.riotgames.com/tft/match/v1/matches/by-puuid/{puu_id}?api_key={api_key['key']}")
-
-        with open('data.txt', 'w') as outfile:
-            json.dump(resp.json(), outfile)
-
-    with open('champions.json') as f:
-        champions = json.load(f)
-
-    champions = champions['data']
-    for champ, info in champions.items():
-        key = info['key']
-        info['key'] = int(key)
-    champs_df = pd.DataFrame(champions)
-    # champs_df.reindex(['key'])
-    champs_df = champs_df.T
-    champs_df.rename(columns={"key": "champion"}, inplace=True)
-    champs_df['champion'] = champs_df['champion'].astype('int64')
-
-    matches = resp.json()['matches']
-    matches_df = pd.DataFrame(matches)
-
-    matches_df.merge(champs_df, how='left', on='champion')
-    print(champs_df.head())
-    print(matches_df.head())
+    puuid = riot_api.get_summoner_id(SUMMONER)
+    match_history = riot_api.get_summoner_match_history(puuid)
+    # Get all match data and save to json files
+    riot_api.get_summoner_match_data(SUMMONER, match_history)
 
 
 if __name__ == '__main__':
--- riot_api.py
+++ riot_api.py
@@ -1,19 +1,73 @@
 import requests
 import json
+import os
+import time
+from ratelimit import limits, sleep_and_retry
 
-with open('secret.json') as f:
-    riot_api_key = json.load(f)['riot-key']
+REGION = "NA"
+RATE_LIMIT = {
+    'per_second': 20,
+    'per_minute': 50
+}
+API_CALLS = 100
+ONE_MINUTE = 120
 
 RIOT_API_URL_BASE = "https://na1.api.riotgames.com/"
 RIOT_API_URL_TFT = RIOT_API_URL_BASE + "tft/"
 
+RIOT_API_URL_BASE = "https://na1.api.riotgames.com/"
+RIOT_API_URL_TFT = RIOT_API_URL_BASE + "tft/"
+RIOT_API_URL_TFTMATCH = "https://americas.api.riotgames.com/tft/match/v1/matches/"
+# get Riot API key
+with open('secret.json') as f:
+    riot_api_key = json.load(f)['riot-key']
+
+@sleep_and_retry
+@limits(calls=API_CALLS, period=ONE_MINUTE)
+def call_api(url):
+    # Need additional error handling for common errors (network error, etc.).
+    _response = requests.get(url)
+    _status = _response.status_code
+
+    if _status != 200:
+        raise Exception('API response: {}'.format(_response.status_code))
+    return _response, _status
+
+
 def get_summoner_id(name):
+    # Any additional error handling needed?
     riot_api_url_summoner = RIOT_API_URL_TFT + 'summoner/v1/summoners/by-name/' + name + '?api_key=' + riot_api_key
-    resp = requests.get(riot_api_url_summoner)
+    resp, stat = call_api(riot_api_url_summoner)
     return resp.json()['puuid']
 
 
 def get_summoner_match_history(id):
+    # Any additional error handling needed?
     riot_api_url_tftmatches = "https://americas.api.riotgames.com/tft/match/v1/matches/by-puuid/" + id + "/ids?count=1000&api_key=" + riot_api_key
-    resp = requests.get(riot_api_url_tftmatches)
-    match_history = list(resp.json())
+    resp, stat = call_api(riot_api_url_tftmatches)
+    return list(resp.json())
+
+
+def get_summoner_match_data(summ, match_history):
+
+    existing_matches = os.listdir(os.path.join('data', summ))
+
+    for match in match_history:
+        match_filename = match + '.json'
+        if match_filename in existing_matches:
+            continue
+            
+        request_url = RIOT_API_URL_TFTMATCH + match + '?api_key=' + riot_api_key
+        response, status = call_api(request_url)
+        # If rate limit reached, we will need to retry. Add back into the queue.
+        if status == 429:
+            print('Rate limit exceeded. Adding to back of queue to retry.')
+            match_history.append(match)
+            print('Sleep for 60 seconds.')
+            time.sleep(60)
+            continue
+        match_info = response.json()
+        filepath = os.path.join('data', summ, match_filename)
+        if not os.path.exists(filepath):
+            with open(filepath, 'w') as f:
+                json.dump(match_info, f)
